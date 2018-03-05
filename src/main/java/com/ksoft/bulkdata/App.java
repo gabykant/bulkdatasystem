42d2b6022312fefefb8c44e79f9f7f8924b78d3a
Kwaye Kant <gabrielkwaye@gmail.com>
2018-03-05 14:52:50 +0100

    Skelton

---
+++ src/main/java/com/ksoft/bulkdata/App.java
@@ -0,0 +1,13 @@
+package com.ksoft.bulkdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
